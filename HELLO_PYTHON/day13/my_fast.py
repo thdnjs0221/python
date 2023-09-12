047230716014f4ac8aacd1bb9e919e46c77233dc
DESKTOP-P7TMH9A\PC-14 <thdnjs0221@naver.com>
2023-09-12 19:43:10 +0900

    09/12 수업

---
+++ HELLO_PYTHON/day13/my_fast.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Form, Request
+import uvicorn
+from starlette.responses import HTMLResponse
+from fastapi.params import Form
+from fastapi.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def forw(request: Request):
+    return templates.TemplateResponse('ajax.html', {'request':request})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
